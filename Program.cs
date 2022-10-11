2c2c4c594fa4c53f8201176a2bea81963c935ad4
Aleksandr <dobrikchita@mail.ru>
2022-10-11 10:46:23 +0900

    add Task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Из массива строк сформируйте массив из строк длина которых меньше либо равна 3 символа
+
+int RL () // READLINE
+{
+    int n;
+    Console.WriteLine("Enter number ");
+    return n = Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintArray(string[] array)
+{
+    Console.Write(" [ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+void FillingArray (string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Enter {i + 1} element array "); 
+        array[i] = Console.ReadLine();
+    }
+    Console.WriteLine("Thanks!");
+}
+
+int CountLenghtString(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length < 4) count += 1;
+    }
+    return count;
+}
+
+void FillingEndArray(string[] array, string[] endArray)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length < 4)
+        {
+            endArray[count] = array[i];
+            count +=1;
+        }
+    }
+}
+
+Console.WriteLine("Hello, enter lenght array string ");
+int lenghtArray = RL();
+string[] newArray = new string[lenghtArray];
+FillingArray(newArray);
+PrintArray (newArray);
+int lenghtEndArray = CountLenghtString(newArray);
+string[] endArray = new string [lenghtEndArray];
+FillingEndArray(newArray, endArray);
+PrintArray (endArray);
